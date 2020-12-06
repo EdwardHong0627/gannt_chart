56ea2b0ccb22acf760961a739190706278f7d7f8
Edward Hong <edwardboy0627@gmail.com>
2020-12-06 12:43:49 +0800

    bug-fixing

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='ganntchart',
-    version='1.0',
+    version='1.1',
     description='Draw Gannt Chart with svg format',
     url='https://github.com/EdwardHong0627/gannt_chart',
     author='EdwardHong0627',
     author_email='edwardboy0627@gmail.com',
     license='MIT',
     zip_safe=False,
-    packages=find_packages(),
+    packages=["ganntchart"],
     keywords=['ganntchart', "scheduling"],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
